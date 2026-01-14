2cd457030cf13979908f039e351ec6dbfad1eca5
Cedric12850 <cedric.deswarte81@gmail.com>
2026-01-14 11:28:50 +0100

    correction formulaire update_progression, bouton ajout d'un élève dans template eleve_list

--- backend/eleves/forms.py
+++ backend/eleves/forms.py
@@ -4,12 +4,13 @@ from ckeditor.widgets import CKEditorWidget
 
 class ProgressionForm(forms.ModelForm):
     commentaire = forms.CharField(widget=CKEditorWidget(), required=False)
+
     class Meta:
         model = Progression
-        fields = ['date_cours','heure_cours', 'commentaire', 'valide']
+        fields = ['date_cours', 'heure_cours', 'commentaire', 'valide']
         widgets = {
-            'date_cours': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'heure_cours': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'date_cours': forms.DateInput(format='%Y-%m-%d',attrs={'type': 'date', 'class': 'form-control'}),
+            'heure_cours': forms.TimeInput(format='%H:%M',attrs={'type': 'time', 'class': 'form-control'}),
             'valide': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
 
@@ -18,5 +19,5 @@ class EleveForm(forms.ModelForm):
         model = Eleve
         fields = ['nom', 'prenom', 'date_naissance', 'neph', 'email', 'telephone']
         widgets = {
-            'date_naissance': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'date_naissance': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date', 'class': 'form-control'}),
         }
