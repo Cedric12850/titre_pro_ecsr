49ad30c3defd8ffddfa73b2431d5daee999713b0
Cedric12850 <cedric.deswarte81@gmail.com>
2026-01-14 15:11:06 +0100

    ajout du template profil

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-
-from .views import signup_view
+from .views import signup_view, profile_view
 
 app_name = "account"
 
@@ -9,4 +8,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="account/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", signup_view, name="signup"),
+    path("profil/", profile_view, name="profile"),  
 ]
